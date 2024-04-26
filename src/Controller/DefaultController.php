8243612f1f121d6bb6074a277fb08e8a85e73843
Bata Zoltán <batazo.dad@gmail.com>
2024-04-26 22:10:19 +0200

    Default controller, apache-pack

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,47 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class DefaultController  extends AbstractController
+{
+    public function __construct()
+    {
+     
+    }
+
+    #[Route('/', "home")]
+    public function defaultAction(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Hello: '.$number.'</body></html>'
+        );
+    }
+
+    #[Route('/default-json', "default-json")]
+    public function tesztJsonAction(): JsonResponse
+    {
+        (array)$data = ['message'=>'OK'];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route('/default-render', "default-render")]
+    public function defaultRenderAction(Request $request): Response
+    {
+        $env_var = $_ENV['TESZT_ENV'];
+        
+        return $this->render('default/defaultrender.html.twig', [
+            'welcome'=>'Hello W',
+            'env_var'=>$env_var
+        ]);
+    }
+
+
+}
