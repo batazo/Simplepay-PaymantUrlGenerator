96f101e2ff9be340dcf7c605849dd21344f74dcc
Bata Zoltán <batazo.dad@gmail.com>
2024-04-27 15:28:31 +0200

    Ref generation

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 use App\Traits\Test\Exponentiation;
 
@@ -22,15 +21,15 @@ class DefaultController  extends AbstractController
     #[Route('/', "home")]
     public function defaultAction(): Response
     {
-        $sum = $this->double(4);//sum(5, 5);
-
         $items = [
             "product"=>"OK",
         ];
 
+        $ref = uniqid(true);
+
         return $this->render('default/home.html.twig', [
             'items'=>$items,
-            'sum'=> $sum
+            'ref'=> $ref
         ]);
     }
 }
